719c60904de11c969f97b6e2c9b493f73470d164
Behatris Fiorentini <bibiafiorentini@gmail.com>
2021-06-05 15:09:57 -0300

    Criando repository imagem

--- app/Repositories/ImagemRepository.php
+++ app/Repositories/ImagemRepository.php
@@ -9,4 +9,28 @@ use App\Repositories\AbstractRepository;
 class ImagemRepository extends AbstractRepository
 {
 
+    /**
+     * Lista imagens pela notícia
+     *
+     * @param int $noticia_id
+     * @return array
+     */
+    public function findByNoticia(int $noticia_id): array
+    {
+        return $this->model::where('noticia_id'. $noticia_id)
+            ->get()
+            ->toArray();
+    }
+
+    /**
+     * Deleta todas as imagens de uma notícia
+     *
+     * @param integer $noticia_id
+     * @return boolean
+     */
+    public function deleteByNoticia(int $noticia_id):bool
+    {
+        return !!$this->model::where('noticia_id', $noticia_id)
+            ->delete();
+    }
 }
